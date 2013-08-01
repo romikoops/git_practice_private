00a02f3f17a7fec0c037d94aefe727a5440ebcaa
alexspot <alex.kycherenko@gmail.com>
2013-08-01 21:25:39 +0300

    added bundler

---
+++ main.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'sexy_settings'
+
+def settings
+  SexySettings::Base.instance()
+end
+
+SexySettings.configure do |config|
+  config.path_to_default_settings = File.expand_path("config.yaml", File.join(File.dirname(__FILE__), '..', 'config')) # 'default.yml' by default
+  config.path_to_custom_settings = File.expand_path("overwritten.yaml", File.join(File.dirname(__FILE__), '..', 'config')) # 'custom.yml' by default
+  config.path_to_project = File.dirname(__FILE__) # '.' by default
+  config.env_variable_with_options = 'OPTIONS' # 'OPTS' by default
+  cmd_line_option_delimiter = '$$$' # ',' by default
+end
