d1f5b4966817743058f24914e30affec00b6d737
alexspot <alex.kycherenko@gmail.com>
2013-08-01 22:34:49 +0300

    msgh

--- main.rb
+++ main.rb
@@ -10,7 +10,7 @@ end
 
 SexySettings.configure do |config|
   config.path_to_default_settings = File.expand_path("config.yaml", File.join(File.dirname(__FILE__), 'config')) # 'default.yml' by default
-  config.path_to_custom_settings = File.expand_path("overwritten.yaml", File.join(File.dirname(__FILE__), '..', 'config')) # 'custom.yml' by default
+  config.path_to_custom_settings = File.expand_path("overwritten.yaml", File.join(File.dirname(__FILE__), 'config')) # 'custom.yml' by default
   config.path_to_project = File.dirname(__FILE__) # '.' by default
   config.env_variable_with_options = 'OPTIONS' # 'OPTS' by default
   cmd_line_option_delimiter = '$$$' # ',' by default
